68458b038885c14cc00eefcc62a4b04dbecb45ca
jsbalrog <ted.jenkins@familysearch.org>
2023-04-18 09:01:06 -0600

    complete exercise 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,4 +1,20 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
+
+const App = () => {
+  return (
+    <div>
+      <Logo />
+      <h1>Bookshelf</h1>
+      <button onClick={() => alert('login')}>Login</button>
+      <button onClick={() => alert('register')}>Register</button>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 // 🐨 you'll also need to import the Logo component from './components/logo'
 
